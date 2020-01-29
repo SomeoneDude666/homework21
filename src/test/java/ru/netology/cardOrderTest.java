d050b6f0f851908bdfa9d2f85ae4dfd869e50ee0
Mihail Galkov <gaus.spb@gmail.com>
2020-01-29 11:49:59 +0300

    issues fix

--- src/test/java/ru/netology/cardOrderTest.java
+++ src/test/java/ru/netology/cardOrderTest.java
@@ -6,11 +6,10 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class cardOrderTest {
+public class CardOrderTest {
     @Test
     void shouldSumbitRequest() {
         open("http://localhost:9999/");
-        //SelenideElement form=$("[form form_size_m form_theme_alfa-on-white]");
         $("[data-test-id=name] input").setValue("Петр Васильев");
         $("[data-test-id=phone] input").setValue("+79266264578");
         $("[data-test-id=agreement]").click();
